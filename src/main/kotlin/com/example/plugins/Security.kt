16b099e8f772fd6b6f7a12253f9a3fc77251a55b
Nikita S <snikitasergeevich@gmail.com>
2023-11-26 22:27:26 +0300

    fix

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -9,18 +9,18 @@ import io.ktor.server.routing.*
 fun Application.configureSecurity() {
     data class MySession(val count: Int = 0)
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
-        }
+//        cookie<MySession>("MY_SESSION") {
+//            cookie.extensions["SameSite"] = "lax"
+//        }
         cookie<ChatReceiveRemote>("CHAT_SESSION") {
             cookie<ChatReceiveRemote>("SESSION")
         }
     }
-    routing {
-        get("/session/increment") {
-                val session = call.sessions.get<MySession>() ?: MySession()
-                call.sessions.set(session.copy(count = session.count + 1))
-                call.respondText("Counter is ${session.count}. Refresh to increment.")
-            }
-    }
+//    routing {
+//        get("/session/increment") {
+//                val session = call.sessions.get<MySession>() ?: MySession()
+//                call.sessions.set(session.copy(count = session.count + 1))
+//                call.respondText("Counter is ${session.count}. Refresh to increment.")
+//            }
+//    }
 }
